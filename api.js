67a98ee2a455fd6cf343d232e5fe11d604601bd2
skulltombstone-hub <skulltombstone@gmail.com>
2026-06-17 20:22:05 -0300

    Create api.js

---
+++ api.js
@@ -0,0 +1,16 @@
+async function analyzeSymptoms(symptoms){
+
+    return `
+Possíveis causas:
+
+• Resfriado comum
+• Gripe
+• Infecção viral leve
+
+Consenso entre modelos:
+82%
+
+Recomendação:
+Monitorar sintomas e procurar atendimento médico se piorarem.
+`;
+}
